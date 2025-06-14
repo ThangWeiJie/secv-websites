779b5f3f29ab072532b8477d90702ce7228beb6f
WeiJie <thangweijie@gmail.com>
2025-06-14 12:47:36 +0800

    Removed db_name

--- Asg3/createDB.php
+++ Asg3/createDB.php
@@ -2,8 +2,7 @@
 $db_host = "localhost";
 $db_user = "root";
 $db_pass = "";
-$db_name = "";
-$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+$conn = mysqli_connect($db_host, $db_user, $db_pass);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
